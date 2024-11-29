fe0746254c9b7b05902bf87bfb15d48541cd6e84
SundayBPM <sundaybontong@gmail.com>
2024-11-29 11:01:58 +0700

    add some project information

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.CharField(max_length=300)
+    tech_stack = models.CharField(max_length=100)
+    content = models.TextField(blank=True, null=True)
+    img = models.CharField(max_length=100, blank=True, null=True)
+    repo = models.CharField(max_length=100, blank=True, null=True)
+    link_post = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from blog.models import Blog
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    """
+    function to show home page
+    """
+    blogs = Blog.objects.all()
+    return render(request, 'blog/home.html', {'blogs': blogs})
+
+def warning(request):
+    """
+    This function used to show information to user that page they trying to access is restricted
+    """
+    return render(request, 'blog/warnings.html')
